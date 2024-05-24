fa2ac377bc0af88520abed3a54c12f00f469641c
Sergio Regalado Alessi <serdigital@live.com.ar>
2024-05-23 22:25:31 -0300

    Add Class N°07 + After Class

---
+++ clase_07_after_class/api_restful/usuarios.js
@@ -0,0 +1,56 @@
+const usuarios = [
+    {
+        id: 1,
+        nombre: "Juan",
+        apellido: "Pérez",
+        edad: 20,
+        correo: "juan@gmail.com",
+        genero: "M",
+    },
+    {
+        id: 2,
+        nombre: "Lorena",
+        apellido: "Medina",
+        edad: 30,
+        correo: "lore@hotmail.com",
+        genero: "F",
+    },
+    {
+        id: 3,
+        nombre: "Maria",
+        apellido: "Pereyra",
+        edad: 25,
+        correo: "mary2024@gmail.com",
+        genero: "F",
+    },
+    {
+        id: 4,
+        nombre: "Mario",
+        apellido: "Pereyra",
+        edad: 22,
+        correo: "mar1980@gmail.com",
+        genero: "M",
+    },
+    {
+        id: 5,
+        nombre: "Pablo",
+        apellido: "Flores",
+        edad: 18,
+        correo: "pablito@gmail.com",
+        genero: "M",
+    }
+];
+
+const generarId = () => {
+    let mayorId = 0;
+
+    usuarios.forEach((usuario) => {
+        if (usuario.id > mayorId) {
+            mayorId = usuarios.id;
+        }
+    });
+
+    return mayorId + 1;
+};
+
+export default { usuarios, generarId };
